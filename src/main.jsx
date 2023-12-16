9adafb81faee8a39eeaee28faa20bbecd52e07a3
mr-kargar <mr.kargar@gmail.com>
2023-12-16 23:21:58 +0330

    Building a store with Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    </Provider>
+  
 )
