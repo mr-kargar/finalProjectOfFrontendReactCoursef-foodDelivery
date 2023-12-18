d5bd1c10d0c98875ecb7f4aa274658336ec93f3f
Amin Kargar <98560519+mr-kargar@users.noreply.github.com>
2023-12-18 21:58:59 +0330

    build signUp form and toggle #2

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -3,21 +3,11 @@ import { useState } from "react";
 import Button from "../components/button";
 
 function LoginPage() {
-  const [emailAddress, setEmailAddress] = useState("");
-  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(true);
 
-  const handleEmailAddressChange = (e) => {
-    setEmailAddress(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-  };
+  function showForm() {
+    setShow(!show);
+  }
 
   return (
     <div className="loginPage">
@@ -28,31 +18,32 @@ function LoginPage() {
             alt="logo"
             className="logo-large"
           />
-          <div className="loginSignUpContainer">
-            <p>Login</p>
-            <p>Sign-up</p>
+          <div className="loginSignUpContainer" onClick={showForm}>
+            <p className={`${show ? "showBorder" : null}`}>Login</p>
+            <p className={`${show ? null : "showBorder"}`}>Sign-up</p>
           </div>
         </div>
 
-        <form className="formContainer" onSubmit={handleSubmit}>
+        <form className={`formContainer loginMargin ${show ? "show" : null}`}>
           <label htmlFor="Email address">Email address:</label>
-          <input
-            type="email"
-            id="emailAddress"
-            value={emailAddress}
-            onChange={handleEmailAddressChange}
-          />
+          <input type="email" id="emailAddress" />
 
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
+          <input type="password" id="password" />
+          <Button label={"Login"} className={"primary"} />
+        </form>
+
+        <form className={`formContainer signUpMargin ${show ? null : "show"}`}>
+          <label htmlFor="Email address">Email address:</label>
+          <input type="email" id="emailAddress" />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
+          <label htmlFor="passwordConfirm">Password Confirm :</label>
+          <input type="password" id="passwordConfirm" />
+          <Button label={"Sign Up"} className={"primary"} />
         </form>
       </div>
-      <Button label={"Login"} className={"primary"} />
     </div>
   );
 }
