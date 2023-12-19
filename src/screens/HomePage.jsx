f400f4994653610de3519872b0be30df30fdc840
Amin Kargar <98560519+mr-kargar@users.noreply.github.com>
2023-12-19 22:44:31 +0330

    build search bar component

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HamburgerMenu from '../components/HamburgerMenu'
+import SearchBar from '../components/SearchBar'
 
 function HomePage() {
   return (
     <div className="homePage">
         <HamburgerMenu />
+        <SearchBar />
+
     </div>
   )
 }
