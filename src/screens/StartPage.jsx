d83b155523a5107dbebd2dca5708a4e4d721524c
Amin Kargar <98560519+mr-kargar@users.noreply.github.com>
2023-12-24 22:06:47 +0330

    Implementation of BEM on the start page

--- src/screens/StartPage.jsx
+++ src/screens/StartPage.jsx
@@ -4,15 +4,15 @@ import Button from "../components/button";
 function startPage() {
   return (
     <div className="startPage">
-      <div>
-        <div className="imageContainer">
-          <img src="src\assets\images\logo.png" alt="logo" className="logo" />
+      <div className="startPage-content">
+        <div className="startPage-content-logoContainer">
+          <img src="src\assets\images\logo.png" alt="logo" className="startPage-content-logoContainer-logo" />
         </div>
         <p>Food for Everyone</p>
         <img
           src="src\assets\images\startPageImage.png"
           alt="startPageImage"
-          className="startPageImage"
+          className="startPage-content-image"
         />
       </div>
       <Button label={"Get started"} className={"get-started"} />
